1d3937c4472255a30b8ec99a541fcf02ede8155f
Sadia Sultana Kumu <kumusadia02@gmail.com>
2023-07-30 01:05:19 +0600

    [feat](navbar design) : folder structure and navbar design done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import IndividualHotel from "./pages/hotel/IndividualHotel";
+import "./scss/main.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotel-list" element={<List/>}/>
+        <Route path="/individual-hotel/:id" element={<IndividualHotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/list/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <div>
+            hotel-list
+        </div>
+    );
+};
+
+export default List;
