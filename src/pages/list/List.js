3669a8b5dd967be1a96b60fd60ee20caded0f229
Sadia Sultana Kumu <kumusadia02@gmail.com>
2023-08-09 01:11:13 +0600

    [ADD] header design complete

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
 
 const List = () => {
     return (
         <div>
-            hotel-list
+            <Navbar/>
+            <Header type="list"/>
         </div>
     );
 };
